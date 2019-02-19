bda459da60af91f161c8a94a6738bac32a8dfb2c
Brendan Brooks <brendan@gobrooks.com>
2019-02-19 10:46:56 -0700

    Fixed the admin-side token to properly destroy
    Former-commit-id: e325ef6cf3208d9255eb3dc39c1f703b7be83916

--- src/brooks/api/controllers/AdminController.java
+++ src/brooks/api/controllers/AdminController.java
@@ -27,6 +27,7 @@ public class AdminController {
 	@RequestMapping(path= { "/", "/main" }, method=RequestMethod.GET)
 	public ModelAndView displayAdminPage() {
 		ModelAndView mv = new ModelAndView();
+		httpSession.removeAttribute("token");
 		httpSession.setAttribute("token", token);
 		mv.setViewName("admin");
 		mv.addObject("item", new ItemModel());
@@ -37,7 +38,7 @@ public class AdminController {
 	public ModelAndView loginUser(@ModelAttribute("item")ItemModel item) {
 		ModelAndView mv = new ModelAndView();
 		if(!tokenIsValid(true)) {
-			mv.setViewName("redirect: main");
+			mv.setViewName("error");
 			return mv;
 		}
 
@@ -47,7 +48,6 @@ public class AdminController {
 		try {
 			status = itemService.addItem(item);
 		} catch (ItemAlreadyExistsException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
@@ -65,22 +65,23 @@ public class AdminController {
 	public ModelAndView loginUser() {
 		ModelAndView mv = new ModelAndView();
 		if(!tokenIsValid(true)) {
-			mv.setViewName("redirect: main");
+			mv.setViewName("error");
 			return mv;
 		}
 		return mv;
 	}
 	
 	private boolean tokenIsValid(boolean deleteTokenAfter) {
-		int retrievedToken = -1;
+		int retrievedToken = 0;
 		if (httpSession.getAttribute("token") != null) {
+			System.out.println("NOT NULL");
 			retrievedToken = (int) httpSession.getAttribute("token");	
 		}
 		if (retrievedToken != token)
 			return false;	
 		if (deleteTokenAfter)
 			httpSession.removeAttribute("token");
-		System.out.println("token is valid");
+		System.out.println("token is valid: " + token);
 		return true;
 	} 
 	
